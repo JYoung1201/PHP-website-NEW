473ae1ecd95e92d603c81661259c3eecb69cb816
JYoung1201 <114886900+JYoung1201@users.noreply.github.com>
2024-08-26 20:08:53 -0500

    New Semester Website
    New website. Domain is now jyoung1201.lol

---
+++ global/menu.php
@@ -0,0 +1,33 @@
+<nav class="navigation">
+        <ul>
+            <!-- <li><a href="indexwithcss.php">Home</a></li>
+            <li><a href="module1variables.php">Organizational Chart</a></li>
+            <li><a href="module2.php">Web Poll</a></li>
+            <li><a href="module3.php">Update Oragnizational Chart</a></li>
+            <li><a href="module4.php">Login</a></li>
+      
+            <li><a href="about.php">About Us</a></li>
+            <li><a href="missions.php">Our Christian Mission</a></li>
+            <li><a href="phpinfo.php">PHP Info</a></li>
+            <li><a href="contact.php">Contact Us</a></li> -->
+    
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about.php">About Us</a></li>
+            <li><a href="phpinfo.php">PHP Info</a></li>
+            <li><a href="contact.php">Contact Us</a></li>
+            <li><a href="variablesnew.php">week 1 variables </a></li>
+            <li><a href="module1variables.php">Module 1: Week 1 Foundations </a></li>
+            <li><a href="module2.php">Module 1: Week 1 Variables</a></li>
+            <li><a href="module3.php">Module 2: Week 2 Forms</a></li>
+            <li><a href="module4.php">Module 3: Week 3 Arrays</a></li>
+            <li><a href="week4sessions.php">Module 4: Week 4 Sessions</a></li>
+           
+            <li><a href="missions.php">Module 5: Week 5 CMS Sessions</a></li>
+   >
+            <li><a href="week6.php">Module 6: Week 6 Database</a></li>
+            <li><a href="week8.php">Module 8: Week 8 CMS Database</a></li>
+
+
+
+        </ul>
+</nav>
